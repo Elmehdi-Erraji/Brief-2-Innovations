97415bd3e7759ec8d294c8a940b1036adc984b77
Mehdi <elmahdi20erraji@gmail.com>
2023-10-22 17:24:15 +0100

    responsive contact, login , signup ,nav footer sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,48 @@
-function myFunction() {
-	document.getElementById("myDropdown").classList.toggle("show");
-  }
+// function myFunction() {
+// 	document.getElementById("myDropdown").classList.toggle("show");
+//   }
 
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-	  var dropdowns = document.getElementsByClassName("dropdown-content");
-	  var i;
-	  for (i = 0; i < dropdowns.length; i++) {
-		var openDropdown = dropdowns[i];
-		if (openDropdown.classList.contains('show')) {
-		  openDropdown.classList.remove('show');
-		}
-	  }
-	}
-  } 
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+// 	if (!event.target.matches('.dropbtn')) {
+// 	  var dropdowns = document.getElementsByClassName("dropdown-content");
+// 	  var i;
+// 	  for (i = 0; i < dropdowns.length; i++) {
+// 		var openDropdown = dropdowns[i];
+// 		if (openDropdown.classList.contains('show')) {
+// 		  openDropdown.classList.remove('show');
+// 		}
+// 	  }
+// 	}
+//   } 
 
 
-  function myFunction1() {
-	document.getElementById("myDropdown1").classList.toggle("show");
-  }
+//   function myFunction1() {
+// 	document.getElementById("myDropdown1").classList.toggle("show");
+//   }
   
-  // Close the dropdown if the user clicks outside of it
-  window.onclick = function(event) {
-	if (!event.target.matches('.dropbtn')) {
-	  var dropdowns = document.getElementsByClassName("dropdown-content");
-	  var i;
-	  for (i = 0; i < dropdowns.length; i++) {
-		var openDropdown = dropdowns[i];
-		if (openDropdown.classList.contains('show')) {
-		  openDropdown.classList.remove('show');
-		}
-	  }
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+// 	if (!event.target.matches('.dropbtn')) {
+// 	  var dropdowns = document.getElementsByClassName("dropdown-content");
+// 	  var i;
+// 	  for (i = 0; i < dropdowns.length; i++) {
+// 		var openDropdown = dropdowns[i];
+// 		if (openDropdown.classList.contains('show')) {
+// 		  openDropdown.classList.remove('show');
+// 		}
+// 	  }
+// 	}
+//   }
+
+
+  function myFunction4() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
 	}
   }
+
 
